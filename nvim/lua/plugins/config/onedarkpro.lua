8358f0f75ca48ec90070ac411255f14b0d86a1e9
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-05-24 19:58:24 +0600

    nvim - changes

--- nvim/lua/plugins/config/onedarkpro.lua
+++ nvim/lua/plugins/config/onedarkpro.lua
@@ -30,38 +30,12 @@ onedarkpro.setup({
     cyan = "#48b0bd",
     blue = "#4fa6ed",
     purple = "#bf68d9",
-    -- purple = "#42d4f5",
     black = "#0e1013",
     gray = "#535965",
-
-    -- bg_statusline = "#282C34",
-    -- fg_gutter = "#323641",
-    -- fg_sidebar = "#8EBD6B"
-
-    -- default_purple = "#BF68D9",
   },
   hlgroups = {
     CursorLineNr = { fg = "NONE" },
-    -- Statement = { fg = "${default_purple}" },
-    -- Conditional = { fg = "${default_purple}" },
-    -- Repeat = { fg = "${default_purple}" },
-    -- Keyword = { fg = "${default_purple}" },
-    -- Exception = { fg = "${default_purple}" },
-    -- Include = { fg = "${default_purple}" },
-    -- Define = { fg = "${default_purple}" },
-    -- Structure = { fg = "${default_purple}" },
-    -- Typedef = { fg = "${default_purple}" },
   },
-  filetype_hlgroups = {
-    -- TSAttribute = { fg = "${default_purple" },
-    -- TSConstBuiltin = { fg = "${default_purple" },
-    -- TSInclude = { fg = "${default_purple" },
-    -- TSKeyword = { fg = "${default_purple" },
-    -- TSKeywordFunction = { fg = "${default_purple" },
-    -- TSKeywordOperator = { fg = "${default_purple" },
-    -- TSLabel = { fg = "${default_purple" },
-    -- TSRepeat = { fg = "${default_purple" },
-  }
 })
 
 onedarkpro.load()
