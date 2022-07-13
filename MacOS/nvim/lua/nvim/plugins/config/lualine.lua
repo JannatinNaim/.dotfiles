2567fe9ad5734abf09d4015c35f5b9d16ca8034b
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-07-13 15:25:02 +0600

    chore[MacOS/nvim]: change lualine style

--- MacOS/nvim/lua/nvim/plugins/config/lualine.lua
+++ MacOS/nvim/lua/nvim/plugins/config/lualine.lua
@@ -77,8 +77,10 @@ lualine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "onedark",
-		section_separators = { left = "", right = "" },
-		component_separators = { left = "", right = "" },
+		-- section_separators = { left = "", right = "" },
+		-- component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		component_separators = { left = "", right = "" },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
